9158ff8d6cbe110c371c94767344c35e8c3ccf13
Aaditya Mehta <92634361+Aadi022@users.noreply.github.com>
2025-09-19 19:04:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/LivePoll-Frontend"
+  //base: process.env.VITE_BASE_PATH || "/LivePoll-Frontend"
 });
+
 
 
